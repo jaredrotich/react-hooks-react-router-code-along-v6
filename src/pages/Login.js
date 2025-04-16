20b780c45f6d176ae4f841f024e14b9d823e7d0f
jared <jaredrotich3@gmail.com>
2025-04-16 12:20:05 +0300

    modified login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,21 @@
+import NavBar from "../components/NavBar";
+
 function Login(){
     return(
         <>
             <header>
-                {/* Save space for navbar */}
+                <NavBar />
             </header>
             <main>
                 <h1>Login</h1>
                 <form>
                     <div>
                         <label for="username">username</label>
-                        <inpu id="username" type="text" name="username" placeholder="username"></inpu>
+                        <input id="username" type="text" name="username" placeholder="username"></input>
                     </div>
                     <div>
                         <label for="Password">Password</label>
-                        <inpu id="password" type="password" name="password" placeholder="password"></inpu>
+                        <input id="password" type="password" name="password" placeholder="password"></input>
                     </div>
                     <br/>
                     <button type="submit">Submit</button>
